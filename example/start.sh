14b85b20a2c9cc3b0b870eb912bad240a48fbf47
Maria Yli-Heikkilä <myliheik@Marias-Laptop.local>
2024-11-18 13:51:46 +0200

    removed all crop yield 2021 files

--- example/start.sh
+++
@@ -1,9 +0,0 @@
-. ../example/config.config
-#echo $startdate
-
-python ../python/pathfinder.py -s $startdate -e $enddate  -d $datapath -b 4
-
-for name in $(cat bandpaths.txt)
-do
-    python ../python/arrayextractor.py -f $name -shp $shppath -p $projectpath -id $idname 
-done
